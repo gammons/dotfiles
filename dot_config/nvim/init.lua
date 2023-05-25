126064b975010011047eb24ca360067449a50363
Grant Ammons <grant@grant.dev>
2023-05-25 07:28:49 -0400

    correctly load this nvim lua file

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -64,20 +64,8 @@ require('packer').startup(function()
   use 'github/copilot.vim'
 end)
 
-local function nvim_tree_on_attach(bufnr)
-  local api = require('nvim-tree.api')
-
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
-  vim.keymap.set('n', 'i', api.node.open.horizontal, opts('Open: Horizontal Split'))
-  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
-  vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-end
-
 -- nvim tree
+require "nvim_tree_on_attach"
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   on_attach = nvim_tree_on_attach,
