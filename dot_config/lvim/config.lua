ede4d5986002710175471af722191a9e550e8d24
Grant Ammons <grant@grant.dev>
2023-06-27 07:42:48 -0400

    set up copilot better

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -13,8 +13,25 @@ lvim.plugins = {
     dependencies = { "zbirenbaum/copilot.lua" },
     config = function()
       vim.defer_fn(function()
-        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+        local has_words_before = function()
+          if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+        end
+
+        require("copilot").setup()
+        local cmp = require("copilot_cmp")
+        cmp.setup({
+          mapping = {
+            ["<Tab>"] = vim.schedule_wrap(function(fallback)
+              if cmp.visible() and has_words_before() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              else
+                fallback()
+              end
+            end),
+          },
+        })
       end, 100)
     end,
   }
