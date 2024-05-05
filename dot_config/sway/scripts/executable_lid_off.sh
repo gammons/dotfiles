97f7bc1610bf7f64ef962d4574614de231b1d0fa
Grant Ammons <grant@grant.dev>
2024-05-05 09:09:12 -0400

    update script

--- dot_config/sway/scripts/executable_lid_off.sh
+++ dot_config/sway/scripts/executable_lid_off.sh
@@ -1,5 +1,5 @@
 swaymsg output eDP-1 enable
-swaymsg output DP-1 scale 1.0 pos 3840 0 res 3840x2160@144Hz
-swaymsg output DP-2 scale 1.0 pos 3840 0 res 3840x2160@144Hz
-swaymsg output DP-3 scale 1.0 pos 3840 0 res 3840x2160@144Hz
+swaymsg output DP-1 scale 1.0 pos 2880 0 res 3840x2160@144Hz
+swaymsg output DP-2 scale 1.0 pos 2880 0 res 3840x2160@144Hz
+swaymsg output DP-3 scale 1.0 pos 2880 0 res 3840x2160@144Hz
 
