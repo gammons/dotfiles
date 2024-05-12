d42a8a22d25b3564cc9cb11f3094d7f8049c7bc0
Grant Ammons <grant@grant.dev>
2024-05-12 06:45:06 -0400

    fix up the lid bug

--- dot_config/sway/scripts/executable_lid_on.sh
+++
@@ -1,4 +0,0 @@
-swaymsg output eDP-1 disable
-swaymsg output DP-1 scale 1.0 pos 0 0 res 3840x2160@144Hz
-swaymsg output DP-2 scale 1.0 pos 0 0 res 3840x2160@144Hz
-swaymsg output DP-2 scale 1.0 pos 0 0 res 3840x2160@144Hz
