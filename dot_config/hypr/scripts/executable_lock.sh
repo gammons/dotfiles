adf41c0b26879ad9070047ccfbc152b75b1758b6
Grant Ammons <grant@grant.dev>
2024-07-23 09:02:39 -0400

    add hypridle / hyprlock

--- dot_config/hypr/scripts/executable_lock.sh
+++ dot_config/hypr/scripts/executable_lock.sh
@@ -1,1 +1,2 @@
-~/.config/hypr/scripts/inhibit.sh && swaylock --screenshots --indicator --grace 2 --fade-in 0.2 --effect-blur 7x5 --effect-vignette 0.5:0.5
+#~/.config/hypr/scripts/inhibit.sh &&
+swaylock --screenshots --indicator --grace 2 --fade-in 0.2 --effect-blur 7x5 --effect-vignette 0.5:0.5
