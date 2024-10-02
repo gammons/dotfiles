cf0b475ff88e40396f33bc5398e5164c272f613b
Grant Ammons <grant@grant.dev>
2024-10-02 07:12:52 -0400

    finally fixup the resume issues

--- dot_config/sway/scripts/executable_resume.sh
+++ dot_config/sway/scripts/executable_resume.sh
@@ -1,1 +1,9 @@
-~/.config/sway/scripts/clamshell
+#!/usr/bin/env bash
+
+LID="`ls /proc/acpi/button/lid | head -n1`"
+
+if grep -q open /proc/acpi/button/lid/${LID}/state; then
+  ~/.config/sway/scripts/lid_up
+else
+  ~/.config/sway/scripts/lid_down
+fi
